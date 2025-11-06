4d432267e191a7eb6e1a9301a491faff6b832a20
AkhilRobo <padidarapuakhilsai@yahoo.com>
2025-11-06 15:20:57 +0530

    Fixed sensor pole

--- panda_description/launch/test_sensor_pole.launch.py
+++ panda_description/launch/test_sensor_pole.launch.py
@@ -12,7 +12,7 @@ from launch.actions import ExecuteProcess, IncludeLaunchDescription
 from launch.substitutions import (
     Command, 
     PathJoinSubstitution, 
-    LaunchConfiguration  # <-- Added
+    LaunchConfiguration 
 )
 
 def generate_launch_description():
@@ -31,7 +31,6 @@ def generate_launch_description():
     
     robot_description = PathJoinSubstitution([
         get_package_share_directory('panda_description'),	
-        #get_package_share_directory('panda_moveit_config'),	
         xacro_path
     ])
     
@@ -51,7 +50,8 @@ def generate_launch_description():
         name='robot_state_publisher',
         output='screen',        
         parameters=[{
-            'robot_description':Command(['xacro ', robot_description] )
+            'robot_description':Command(['xacro ', robot_description] ),
+            'use_sim_time': use_sim_time
         }]
     )
 
@@ -59,13 +59,10 @@ def generate_launch_description():
 
     workspace_path = os.environ.get('COLCON_PREFIX_PATH') or os.environ.get('AMENT_PREFIX_PATH')
 
-# panda description share dir
     pkg_panda_description = os.path.join(workspace_path, "panda_description", "share")
 
-# default gazebo models location
     gz_models_path = "/home/akhil/.ignition/models"
 
-# set search path for resources
     gz_resource_path = SetEnvironmentVariable(
     name="GZ_SIM_RESOURCE_PATH",
     value=[f"{pkg_panda_description}:{gz_models_path}"]
@@ -80,22 +77,12 @@ def generate_launch_description():
                             output='screen')
 
     
-    # load_joint_state_broadcaster = ExecuteProcess(
-    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-    #          'joint_state_broadcaster'],
-    #     output='screen'
-    # )
-
-    
     
 
     sdf_file_path = os.path.join(
         FindPackageShare('panda_description').find('panda_description'),
         'world',
-        # 'planning_world.sdf'
-        # 'pr2_perception.world'
         'Mainworld.sdf'
-        # planning_world.sdf
         
     )
     ignition_gazebo_node = IncludeLaunchDescription(
@@ -113,32 +100,6 @@ def generate_launch_description():
         }.items()
     )
 
-    # load_position_controller = ExecuteProcess(
-    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-    #          'arm_controller'],
-    #     output='screen'
-    # )
-
-    # load_eef_controller = ExecuteProcess(
-    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-    #          'eef_controller'],
-    #     output='screen'
-    # )
-    
-    color_camera_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
-			name = 'color_camera_bridge',
-			output='screen',
-			parameters=[{
-				'use_sim_time': True
-			}],
-			arguments = [
-				'/color_camera' + '@sensor_msgs/msg/Image' + '[ignition.msgs.Image'
-			],
-			remappings = [
-				('/color_camera', '/color_camera')
-			])
-    
-    # 1️⃣1️⃣ Clock bridge
     clock_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -148,45 +109,33 @@ def generate_launch_description():
         arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'],
     )
 
-    depth_camera_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
-			name = 'depth_camera_bridge',
-			output='screen',
-			parameters=[{
-				'use_sim_time': True
-			}],
-			arguments = [
-				'/depth_camera' + '@sensor_msgs/msg/Image' + '[ignition.msgs.Image',
-				'/depth_camera/points' + '@sensor_msgs/msg/PointCloud2' + '[ignition.msgs.PointCloudPacked'
-			],
-			remappings = [
-				('/depth_camera', '/depth_camera'),
-				('/depth_camera/points', '/depth_camera/points')
-			])
-#     world_tf = Node(
-#     package='tf2_ros',
-#     executable='static_transform_publisher',
-#     name='world_tf',
-#     arguments=['0', '0', '0', '0', '0', '0', 'world', 'link_1']
-# )
+    gz_bridge_node = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/model/panda/pose@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V",
+            
+            "/camera/image@sensor_msgs/msg/Image[gz.msgs.Image",
+            "/camera/depth_image@sensor_msgs/msg/Image[gz.msgs.Image",
+            "/camera/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
+            "/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo"
+        ],
+        parameters=[{'use_sim_time': use_sim_time}], 
+    )
 
-    depth_cam_data2cam_link_tf = Node(package='tf2_ros',
-                     executable='static_transform_publisher',
-                     name='cam3Tolink',
-                     output='log',
-                     arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'camera_link', 'panda/link_1/d435_depth'])
+    depth_cam_data2cam_link_tf = Node(
+    package='tf2_ros',
+    executable='static_transform_publisher',
+    name='camera_tf',
+    output='screen',
+    arguments=[
+        '0', '0', '0',
+        '0', '0', '0',         
+        'camera_link_optical',               
+        'panda/link_1/my_rgbd_camera'
+    ]
+)
 
-#     depth_cam_data2cam_link_tf = Node(
-#     package='tf2_ros',
-#     executable='static_transform_publisher',
-#     name='cam3Tolink',
-#     output='log',
-#     arguments=[
-#         '0', '0', '0',      # XYZ offset
-#         '0', '0', '0',      # RPY rotation
-#         'camera_link',      # parent frame
-#         'camera_depth_optical_frame'   # child frame
-#     ]
-# )
 
 
     ld.add_action(use_sim_time_arg)
@@ -198,14 +147,13 @@ def generate_launch_description():
     ld.add_action( robot_state_publisher_node )
     ld.add_action( spawn_node )
 
-    # ld.add_action( load_joint_state_broadcaster )
-    # ld.add_action( load_position_controller )  
-    # ld.add_action( load_eef_controller )  
-         
-    ld.add_action( color_camera_bridge )
-    ld.add_action( depth_camera_bridge )
-    # ld.add_action( world_tf )
+
+
+    
+    ld.add_action( gz_bridge_node )
     ld.add_action( depth_cam_data2cam_link_tf )
+
+
     ld.add_action(clock_bridge) 
 
 
