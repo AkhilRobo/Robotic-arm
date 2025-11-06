92446bb10541fea582e9ec485d56411a917ceaf1
AkhilRobo <padidarapuakhilsai@yahoo.com>
2025-11-06 15:48:33 +0530

    different robot_description for sensor pole

--- panda_description/launch/test_sensor_pole.launch.py
+++ panda_description/launch/test_sensor_pole.launch.py
@@ -1,4 +1,3 @@
-
 import os
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
@@ -29,20 +28,20 @@ def generate_launch_description():
 
     xacro_path = 'urdf/PoleSensor.urdf.xacro'
     
-    robot_description = PathJoinSubstitution([
-        get_package_share_directory('panda_description'),	
+    robot_description_path = PathJoinSubstitution([
+        get_package_share_directory('panda_description'),   
         xacro_path
     ])
     
     set_prime_offload = SetEnvironmentVariable(
-    name='__NV_PRIME_RENDER_OFFLOAD',
-    value='1'
-)
+        name='__NV_PRIME_RENDER_OFFLOAD',
+        value='1'
+    )
 
     set_glx_vendor = SetEnvironmentVariable(
-    name='__GLX_VENDOR_LIBRARY_NAME',
-    value='nvidia'
-)
+        name='__GLX_VENDOR_LIBRARY_NAME',
+        value='nvidia'
+    )
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -50,41 +49,36 @@ def generate_launch_description():
         name='robot_state_publisher',
         output='screen',        
         parameters=[{
-            'robot_description':Command(['xacro ', robot_description] ),
+            'robot_description': Command(['xacro ', robot_description_path]),
             'use_sim_time': use_sim_time
-        }]
+        }],
+        remappings=[
+            ('/robot_description', '/sensor_pole')
+        ]
     )
 
-
-
     workspace_path = os.environ.get('COLCON_PREFIX_PATH') or os.environ.get('AMENT_PREFIX_PATH')
-
     pkg_panda_description = os.path.join(workspace_path, "panda_description", "share")
-
     gz_models_path = "/home/akhil/.ignition/models"
 
     gz_resource_path = SetEnvironmentVariable(
-    name="GZ_SIM_RESOURCE_PATH",
-    value=[f"{pkg_panda_description}:{gz_models_path}"]
-)
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[f"{pkg_panda_description}:{gz_models_path}"]
+    )
 
-    # Spawn
     spawn_node = Node(package='ros_gz_sim', executable='create',
-                 arguments=['-name', 'panda', '-topic', '/robot_description' ,
+                 arguments=['-name', 'sensor_pole', '-topic', '/sensor_pole' ,
                             '-x', '0.438782',
                             '-y', '0.134319',
                             '-z', '0.775935'], 
-                            output='screen')
-
-    
-    
+                 output='screen')
 
     sdf_file_path = os.path.join(
         FindPackageShare('panda_description').find('panda_description'),
         'world',
         'Mainworld.sdf'
-        
     )
+    
     ignition_gazebo_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution([
@@ -96,7 +90,6 @@ def generate_launch_description():
         launch_arguments={
             'gz_args': f'-r -v 4 {sdf_file_path}',
             'use_sim_time': use_sim_time 
-
         }.items()
     )
 
@@ -113,7 +106,7 @@ def generate_launch_description():
         package="ros_gz_bridge",
         executable="parameter_bridge",
         arguments=[
-            "/model/panda/pose@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V",
+            "/model/sensor_pole/pose@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V",
             
             "/camera/image@sensor_msgs/msg/Image[gz.msgs.Image",
             "/camera/depth_image@sensor_msgs/msg/Image[gz.msgs.Image",
@@ -124,37 +117,30 @@ def generate_launch_description():
     )
 
     depth_cam_data2cam_link_tf = Node(
-    package='tf2_ros',
-    executable='static_transform_publisher',
-    name='camera_tf',
-    output='screen',
-    arguments=[
-        '0', '0', '0',
-        '0', '0', '0',         
-        'camera_link_optical',               
-        'panda/link_1/my_rgbd_camera'
-    ]
-)
-
-
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='camera_tf',
+        output='screen',
+        arguments=[
+            '0', '0', '0',
+            '0', '0', '0',         
+            'camera_link',
+                      
+            'sensor_pole/link_1/my_rgbd_camera'
+        ]
+    )
 
     ld.add_action(use_sim_time_arg)
     ld.add_action(set_prime_offload)
     ld.add_action(set_glx_vendor)
-    ld.add_action( gz_resource_path )
-    ld.add_action( ignition_gazebo_node )
+    ld.add_action(gz_resource_path)
+    ld.add_action(ignition_gazebo_node)
 
-    ld.add_action( robot_state_publisher_node )
-    ld.add_action( spawn_node )
-
-
-
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(spawn_node)
     
-    ld.add_action( gz_bridge_node )
-    ld.add_action( depth_cam_data2cam_link_tf )
-
-
+    ld.add_action(gz_bridge_node)
+    ld.add_action(depth_cam_data2cam_link_tf)
     ld.add_action(clock_bridge) 
-
 
     return ld
